30907407b39448f2d0d0a2da0fae6d34b5c20a98
Christian Dionisio <christiandionisio9432@gmail.com>
2020-09-10 00:05:20 -0500

    Este si

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static("/"));
-app.get('/*',function(req,res){
-    res.sendFile(path.join('/index.html'));
+app.use(express.static(__dirname+'/dist/frontendDAD'));
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/frontendDAD'));
 });
 
 app.listen(process.env.PORT || 8080);
